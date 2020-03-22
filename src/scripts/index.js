b047a283bbeaf0e67649629d5dd920a2135e2c64
agumusay <a.gumusay@protonmail.com>
2020-03-22 21:03:23 +0100

    update first li name

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,17 +8,38 @@ import Faker from "faker";
 let listCount = 4;
 let listItems = [];
 //Loop how many list items you want to create
+
 while (listCount) {
   //Create Random information with Faker
   let randomName = Faker.name.findName();
   let randomEmail = Faker.internet.email();
   let randomPhone = Faker.phone.phoneNumber();
   //Create a list elelement on every iteration
-  let listItem = React.createElement("li", {}, randomName, React.createElement("a", {}, randomPhone), React.createElement("a", {}, randomEmail));
+  let listItem = React.createElement(
+    "li",
+    {key:listCount},
+    React.createElement("h4", {}, randomName),
+    React.createElement("a", {}, randomPhone),
+    React.createElement("a", {}, randomEmail)
+  );
   listItems.push(listItem);
   listCount--;
 }
 //Create ul element add a header and span inside, and finally listItems array
-let listContainer = React.createElement("ul", {}, React.createElement("h1", {}, "My ", React.createElement("span", { className: "highLight" }, "awesome "), "contacts list"), listItems);
+let listContainer = React.createElement(
+  "ul",
+  {},
+  React.createElement(
+    "h1",
+    {},
+    "My ",
+    React.createElement("span", { className: "highLight" }, "awesome "),
+    "contacts list"
+  ),
+  listItems
+);
 //append the Ul element inside the container
 ReactDom.render(listContainer, document.querySelector(".container"));
+setInterval(() => {
+  document.querySelector("li h4").innerText = Faker.name.findName();
+}, 1000);
